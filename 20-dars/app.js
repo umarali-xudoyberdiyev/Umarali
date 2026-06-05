50b509fcbd5ca5529e8748b5641235c02826b0d8
Umarali <uz.umarali.dev@gmail.com>
2026-06-05 13:39:57 +0300

    20-dars

---
+++ 20-dars/app.js
@@ -0,0 +1,10 @@
+function NechtaSon(son) {
+  son.toString();
+  return son.lengis;
+}
+
+console.log(NechtaSon(123)); ///3
+console.log(NechtaSon(56)); //2
+console.log(NechtaSon(7154)); //4
+console.log(NechtaSon(61217311514)); //11
+console.log(NechtaSon(0)); //(0)
