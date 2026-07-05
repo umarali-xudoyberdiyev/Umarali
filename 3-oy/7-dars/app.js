de7fa03dd20603291358cf4a1a1bae582217691e
Umarali <uz.umarali.dev@gmail.com>
2026-07-05 19:49:38 +0300

    add function

---
+++ 3-oy/7-dars/app.js
@@ -0,0 +1,28 @@
+const wrapper = document.querySelector(".wrapper");
+const count = document.querySelector(".count");
+
+let cart = 0;
+
+function updateUI(arr) {
+  wrapper.innerHTML = "";
+
+  arr.forEach((item) => {
+    wrapper.innerHTML += `
+      <div class="card">
+        <img src="${item.thumbnail}">
+        <h3>${item.title}</h3>
+        <p>$${item.price}</p>
+        <button onclick="buy()">Buy</button>
+      </div>
+    `;
+  });
+}
+
+function buy() {
+  cart++;
+  count.textContent = cart;
+}
+
+fetch("https://dummyjson.com/products")
+  .then((res) => res.json())
+  .then((data) => updateUI(data.products));
