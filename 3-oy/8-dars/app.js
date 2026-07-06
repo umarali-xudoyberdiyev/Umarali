f8b07d4906540f4f221c182447f78f2bc55bf150
Umarali <uz.umarali.dev@gmail.com>
2026-07-06 13:53:53 +0300

    add function

--- 3-oy/8-dars/app.js
+++ 3-oy/8-dars/app.js
@@ -12,4 +12,46 @@ async function getTodos() {
   }
 }
 
-getTodos();
+// getTodos();
+
+async function postTodos() {
+  let userLogin = {
+    usersId: 1,
+    Id: 1,
+    title: " ",
+    body: " ",
+  };
+  try {
+    let res = await fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(userLogin),
+    });
+    if (!res.ok) {
+      throw new Error("post qilishda muammo");
+    }
+    let data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// postTodos();
+
+async function deletePost(id) {
+  try {
+    let res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+      method: "DELETE",
+    });
+    if (res.status == 200 || res.status == 204) {
+      console.log("ochirildi");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// deletePost(2);
