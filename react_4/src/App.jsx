ef4ba072a09e1da554b5b60a7b1f889a220a3c52
Umarali <uz.umarali.dev@gmail.com>
2026-07-22 13:21:36 +0300

    react_4

--- react_4/src/App.jsx
+++ react_4/src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { FaHome } from "react-icons/io5";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="text-9xl">
+      <FaHome />
+    </div>
+  );
 }
 
 export default App;
